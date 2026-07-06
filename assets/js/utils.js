9b57cf6b76a8d9f0ba2c5b55688f52d45d09c308
mertastudios <sandrorobloxcreations@gmail.com>
2026-07-06 09:21:19 +0200

    utils.js erstellen

---
+++ assets/js/utils.js
@@ -0,0 +1,19 @@
+// HTML-escapen ist wichtig, da Servernamen frei von den Discord-Nutzern gewaehlt werden
+// und direkt per innerHTML eingefuegt werden.
+export function escapeHtml(value) {
+  if (value == null) return '';
+  return String(value)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
+
+export function debounce(fn, ms) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), ms);
+  };
+}
